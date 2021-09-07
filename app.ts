04c964ef7522e6f83c3b7692c9d7dd8413dae262
muhammad_bilal <muhammad.bilal@tintash.com>
2021-09-07 16:10:45 +0500

    added swagger documentation

--- app.ts
+++ app.ts
@@ -2,7 +2,10 @@ import * as dotenv from "dotenv";
 import express from "express";
 import { postRouter } from "./routes/post.routes";
 import { userRouter } from "./routes/user.routes";
+const cors = require("cors");
 import mongoose from "mongoose";
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./docs/swagger.json");
 
 dotenv.config();
 
@@ -17,6 +20,9 @@ const app = express();
 app.use(express.json());
 app.use(postRouter);
 app.use(userRouter);
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors());
 
 const options: Object = {
   useNewUrlParser: true,
